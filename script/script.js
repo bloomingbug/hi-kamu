e81ca1b135bef87c7bf5b13cb40e3f2bec3ae900
tarmujidev <tarmuji1514@gmail.com>
2022-02-19 16:56:26 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,41 @@
+const nama = prompt("Tulis nama kamu dulu dong ☺️");
+const tombolMau = document.getElementById("mau");
+const tombolGak = document.getElementById("gak");
+const namaKamu = document.getElementById("namakamu");
+const gambarTembak = document.getElementById("tembak");
+const paragraph = document.getElementsByTagName("p")[0];
+
+namaKamu.innerHTML = nama;
+
+tombolMau.addEventListener("click", function () {
+  gambarTembak.setAttribute(
+    "src",
+    "https://c.tenor.com/r2NutC5DjRkAAAAM/cute-in-love.gif"
+  );
+});
+
+tombolMau.addEventListener("click", function () {
+  gambarTembak.setAttribute(
+    "src",
+    "https://c.tenor.com/r2NutC5DjRkAAAAM/cute-in-love.gif"
+  );
+  const teks = "I Love You";
+  paragraph.innerHTML = teks;
+  tombolGak.style.position = "static";
+});
+
+tombolGak.addEventListener("click", function () {
+  gambarTembak.setAttribute(
+    "src",
+    "https://c.tenor.com/CfAxiZhADyoAAAAS/quby-chan-crying.gif"
+  );
+  const teks = "Makasi :'(";
+  paragraph.innerHTML = teks;
+
+  const i = Math.floor(Math.random() * 300) + 1;
+  const j = Math.floor(Math.random() * 300) + 1;
+
+  this.style.position = "absolute";
+  this.style.left = i + "px";
+  this.style.top = j + "px";
+});
